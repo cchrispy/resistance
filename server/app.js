df0a8ae34b04aa66678edd589ff2b474aacc041f
Chris Chiang <cchiang1992@gmail.com>
2016-12-22 16:28:48 -0800

    Simple server serving static files set up

---
+++ server/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+const port = process.env.PORT || 8888;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', express.static(path.join(__dirname, '../dist')));
+
+app.listen(port);
+console.log(`Listening on 127.0.0.1:${port}`);
