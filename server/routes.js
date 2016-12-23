283ee4d431bdbea71f07f83f611b1613b7e0746c
Chris Chiang <cchiang1992@gmail.com>
2016-12-22 17:52:39 -0800

    Sample cache system will work

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
 var router = require('express').Router();
 
+var cache = ['apple', 'banana', 'orange', 'eggplant', 'pear', 'peach'];
+
 router.use((req, res, next) => {
   /*
   **  Middle ware activity
@@ -12,7 +14,7 @@ router.get('/', (req, res) => {
   **  Handle GET request
   */
   // res.end();
-  res.status(200).json({ message: 'hi' });
+  res.status(200).json({ message: cache[Math.floor(Math.random() * 6)] });
 });
 
 router.post('/', (req, res) => {
