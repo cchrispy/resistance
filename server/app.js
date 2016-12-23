de3efa2c510f001769ea2e1cb808ac3434ee0383
Chris Chiang <cchiang1992@gmail.com>
2016-12-22 17:02:52 -0800

    Setup game routes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+var router = require('./routes');
+
 const port = process.env.PORT || 8888;
 
 var app = express();
@@ -9,7 +11,9 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/game', router);
 app.use('/', express.static(path.join(__dirname, '../dist')));
 
 app.listen(port);
-console.log(`Listening on 127.0.0.1:${port}`);
+console.log(`Listening on 127.0.0.1:${ port }`);
+
---
+++ server/routes.js
@@ -0,0 +1,26 @@
+var router = require('express').Router();
+
+router.use((req, res, next) => {
+  /*
+  **  Middle ware activity
+  */
+  next();
+});
+
+router.get('/', (req, res) => {
+  /*
+  **  Handle GET request
+  */
+  // res.end();
+  res.json({ sup: 'yo' });
+});
+
+router.post('/', (req, res) => {
+  /*
+  **  Handle POST request
+  */
+  res.json({ waddup: 'dawg' });
+});
+
+module.exports = router;
+
