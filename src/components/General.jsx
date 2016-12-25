f665784ba1dc2799e06a91d94f87786ac3db64d6
Chris Chiang <cchiang1992@gmail.com>
2016-12-24 18:43:10 -0800

    Forms submittal some functionality

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -5,25 +5,62 @@ class General extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newRoomForm: false,
-      joinRoomForm: false
+      newRoomFormVisibility: false,
+      joinRoomFormVisibility: false,
+      newRoomForm: {
+        username: '',
+        roomname: '',
+        playerCount: 5
+      },
+      joinRoomForm: {
+        username: '',
+        roomname: ''
+      }
     };
+  }
+
+  newRoomToggle(e) {
+    e.preventDefault();
+    this.setState({
+      newRoomFormVisibility: !this.state.newRoomFormVisibility,
+      joinRoomFormVisibility: false
+    })
+  }
+
+  joinRoomToggle(e) {
+    e.preventDefault();
+    this.setState({
+      newRoomFormVisibility: false,
+      joinRoomFormVisibility: !this.state.joinRoomFormVisibility
+    })
+  }
+
+  newRoomForm(e, input) {
+    e.preventDefault();
+    this.setState({
+      newRoomForm: {
+        [input]: e.target.value
+      }
+    })
+  }
+
+  joinRoomForm(e, input) {
+    e.preventDefault();
+    this.setState({
+      joinRoomForm: {
+        [input]: e.target.value
+      }
+    })
   }
 
   newRoom(e) {
     e.preventDefault();
-    this.setState({
-      newRoomForm: !this.state.newRoomForm,
-      joinRoomForm: false
-    })
+    console.log(this.state.newRoomForm);
   }
 
   joinRoom(e) {
     e.preventDefault();
-    this.setState({
-      newRoomForm: false,
-      joinRoomForm: !this.state.joinRoomForm
-    })
+    console.log(this.state.joinRoomForm);
   }
 
   render() {
@@ -31,22 +68,30 @@ class General extends Component {
       <div id='general'>
         <div>
 
-          <button type='button' className='btn btn-lg btn-block btn-primary' onClick={ this.newRoom.bind(this) } >
+          <button type='button' className='btn btn-lg btn-block btn-primary' onClick={ this.newRoomToggle.bind(this) } >
             Create a room
           </button>
-          { !this.state.newRoomForm ? null :
-            <form className='form-inline'>
+          { !this.state.newRoomFormVisibility ? null :
+            <form className='form-inline' onSubmit={ this.newRoom.bind(this) }>
               <div className='form-group'>
                 <label>Username: </label>
-                <input type='text' className='form-control' placeholder={ Math.random() > 0.25 ? 'Schwarzenegger' : 'Keanu Reeves' } />
+                <input type='text' 
+                       className='form-control' 
+                       placeholder={ Math.random() > 0.25 ? 'Schwarzenegger' : 'Keanu Reeves' } 
+                       onChange={ (e) => this.newRoomForm(e, 'username') } />
               </div>
               <div className='form-group'>
                 <label>Room name: </label>
-                <input type='text' className='form-control' placeholder={ Math.random() > 0.25 ? 'Planet Earth' : 'Lala Land' } />
+                <input type='text' 
+                       className='form-control' 
+                       placeholder={ Math.random() > 0.25 ? 'Planet Earth' : 'Lala Land' }
+                       onChange={ (e) => this.newRoomForm(e, 'roomname') } />
               </div>
               <div className='form-group'>
                 <label>Number of Players: </label>
-                <select className='form-control' placeholder='5' >
+                <select className='form-control' 
+                        placeholder='5' 
+                        onChange={ (e) => this.newRoomForm(e, 'playerCount') } >
                   <option>5</option>
                   <option>6</option>
                   <option>7</option>
@@ -54,26 +99,32 @@ class General extends Component {
                   <option>9</option>
                   <option>10</option>
                 </select>
-                <button type='button' className='btn btn-default'>
+                <button type='submit' className='btn btn-default'>
                   Create
                 </button>
               </div>
             </form>
           }
 
-          <button type='button' className='btn btn-lg btn-block btn-success' onClick={ this.joinRoom.bind(this) } >
+          <button type='button' className='btn btn-lg btn-block btn-success' onClick={ this.joinRoomToggle.bind(this) } >
             Join a room
           </button>
           {
-            !this.state.joinRoomForm ? null :
+            !this.state.joinRoomFormVisibility ? null :
             <form className='form-inline'>
               <div className='form-group'>
                 <label>Username: </label>
-                <input type='text' className='form-control' placeholder={ Math.random() > 0.25 ? 'Schwarzenegger' : 'Keanu Reeves' } />
+                <input type='text' 
+                       className='form-control' 
+                       placeholder={ Math.random() > 0.05 ? 'Schwarzenegger' : 'Keanu Reeves' }
+                       onChange={ (e) => this.joinRoomForm(e, 'username') } />
               </div>
               <div className='form-group'>
                 <label>Room name: </label>
-                <input type='text' className='form-control' placeholder={ Math.random() > 0.25 ? 'Planet Earth' : 'Lala Land' } />
+                <input type='text' 
+                       className='form-control' 
+                       placeholder={ Math.random() > 0.05 ? 'Planet Earth' : 'Lala Land' }
+                       onChange={ (e) => this.joinRoomForm(e, 'roomname') } />
               </div>
               <button type='button' className='btn btn-default'>
                 Join
